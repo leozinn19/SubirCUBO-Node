01207def5215bb21f3e89d7f05ccb7038dfcdcf6
Leonardo Oliveira <leonardo@maviclik.com>
2023-03-29 12:35:56 -0300

    Novo método de busca de ano concluido

--- test2.js
+++ test2.js
@@ -6,7 +6,8 @@ const wb1 = XLSX.readFile(
   'C:/Users/leona/Desktop/Clientes - Desktop/Reckitt/Bases Originais/BISTEK/2022/Bistek setembro 22.xlsx'
 );
 const ws1 = wb1.Sheets['Base limpeza setembro 2022'];
-let headerN = 0;
+let headerMes = 0;
+let headerAno = 0;
 
 const range = XLSX.utils.decode_range(ws1['!ref']);
 
@@ -16,18 +17,35 @@ for (let c = range.s.c; c <= range.e.c; c++) {
   if (cell) {
     const cellValue = cell.v.toString().toLocaleUpperCase();
     const cellPosition = XLSX.utils.decode_cell(cellAddress);
-    if (cellValue === 'SET') {
-      headerN = cellPosition.c + 1;
+    console.log(cellValue);
+    const anos = ['2020', '2021', '2022', '2023', '2024'];
+    const meses = [
+      'JAN',
+      'FEV',
+      'MAR',
+      'ABR',
+      'MAI',
+      'JUN',
+      'JUL',
+      'AGO',
+      'SET',
+      'OUT',
+      'NOV',
+      'DEZ',
+    ];
+    if (meses.includes(cellValue)) {
+      headerMes = cellPosition.c;
+    }
+    if (anos.includes(cellValue)) {
+      headerAno = cellPosition.c + 1;
     }
   }
 }
 
-function getMes(headerN) {
-  console.log(headerN);
+function getMes(headerMes) {
   const column = XLSX.utils
-    .sheet_to_json(ws1, { header: headerN - 1 })
-    .map((row) => row[headerN - 1].toUpperCase());
-  console.log(column);
+    .sheet_to_json(ws1, { header: headerMes })
+    .map((row) => row[headerMes].toUpperCase());
   XLSX.utils.sheet_add_aoa(
     ws2,
     column.map((value) => [value]),
@@ -36,13 +54,25 @@ function getMes(headerN) {
     }
   );
 }
+function getAno(headerAno) {
+  const column = XLSX.utils
+    .sheet_to_json(ws1, { header: headerAno })
+    .map((row) => row[headerAno - 1]);
+  console.log(column);
+  XLSX.utils.sheet_add_aoa(
+    ws2,
+    column.map((value) => [value]),
+    { origin: 'P1' }
+  );
+}
 
 // Carrega o arquivo de destino
 const destino = 'C:/Users/leona/Desktop/TESTANTO_MODELO.xlsx';
 const wb2 = XLSX.utils.book_new();
 const ws2 = XLSX.utils.aoa_to_sheet([[]]);
 
-getMes(headerN);
+getMes(headerMes);
+getAno(headerAno);
     
 
 
