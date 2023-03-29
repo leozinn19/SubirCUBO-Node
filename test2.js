0ba00c47afb522290009f24508440d9feea7eeb2
Leonardo Oliveira <leonardo@maviclik.com>
2023-03-29 14:55:13 -0300

    Novo Concat e periodo

--- test2.js
+++ test2.js
@@ -41,7 +41,7 @@ for (let c = range.s.c; c <= range.e.c; c++) {
     }
   }
 }
-
+// MES
 function getMes(headerMes) {
   const column = XLSX.utils
     .sheet_to_json(ws1, { header: headerMes })
@@ -54,15 +54,52 @@ function getMes(headerMes) {
     }
   );
 }
+// ANO
 function getAno(headerAno) {
   const column = XLSX.utils
     .sheet_to_json(ws1, { header: headerAno })
     .map((row) => row[headerAno - 1]);
-  console.log(column);
   XLSX.utils.sheet_add_aoa(
     ws2,
     column.map((value) => [value]),
     { origin: 'P1' }
+  );
+}
+// PERIODO
+function getPeriodo(headerMes) {
+  const column = XLSX.utils
+    .sheet_to_json(ws1, { header: headerMes })
+    .map((row) => row[headerMes].toUpperCase());
+  XLSX.utils.sheet_add_aoa(
+    ws2,
+    (column.map((value) => {
+        let result = [value].toString();
+        if (result === 'JAN' || result === 'FEV') return ['1 BIM'];
+        else if (result === 'MAR' || result === 'ABR') return ['2 BIM'];
+        else if (result === 'MAI' || result === 'JUN') return ['3 BIM'];
+        else if (result === 'JUL' || result === 'AGO') return ['4 BIM'];
+        else if (result === 'SET' || result === 'OUT') return ['5 BIM'];
+        else if (result === 'NOV' || result === 'DEZ') return ['6 BIM'];
+      })
+    ),
+    { origin: 'Q1' }
+  );
+  }
+
+// CONCAT
+function concat(headerMes, headerAno) {
+  const column = XLSX.utils
+    .sheet_to_json(ws1, { header: headerMes })
+    .map((row) => row[headerMes].toUpperCase());
+  const column2 = XLSX.utils
+    .sheet_to_json(ws1, { header: headerAno })
+    .map((row) => row[headerAno - 1]);
+
+  const concatenated = column.map((value, index) => value + column2[index]);
+  XLSX.utils.sheet_add_aoa(
+    ws2,
+    concatenated.map((value) => [value]),
+    { origin: 'W1' }
   );
 }
 
@@ -73,6 +110,8 @@ const ws2 = XLSX.utils.aoa_to_sheet([[]]);
 
 getMes(headerMes);
 getAno(headerAno);
+concat(headerMes, headerAno);
+getPeriodo(headerMes);
     
 
 
